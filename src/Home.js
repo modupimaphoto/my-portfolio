77b259911e63f3526aa1f94c5fdcb849ccccf7fe
modupi <111417722+modupimaphoto@users.noreply.github.com>
2023-07-18 17:18:28 +0000

    setup

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,4 @@
-import Header from './Header';
-import About from './About';
-import Projects from './Projects';
-import Footer from './Footer';
-
-import avatar from './images/avatar.svg';
-import devices from './images/devices.svg';
-
-
 const Home = () => {
-    return(
-        <>
-            <Header />
-
-            <section className='showcase-wrapper'>
-                <div className='container'>
-                    
-                </div>
-            </section>
-
-            <About />
-            <Projects />
-            <Footer />
-        </>
-    )
-}
+  return <></>;
+};
 export default Home;
